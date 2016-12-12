9254f038d0f4c4b92676afd1b81f8b4f63f5027b
LeoMcNaughton <9lmcnaughton@stpatrick.org>
2016-12-12 10:53:21 -0600

    Today I created Item

---
+++ First-Semester-Final-master/item.js
@@ -0,0 +1,14 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,s){
+  this.name = n;
+  this.price = p;
+  this.shipping = s
+}
+
+}
+
+//Create your three test items below this comment.
+var TV = new Item("FlemTV", 6000.00, "10 Business Days")
+var Console = new Item("FlemConsole", 5000.00, "5 Business Days")
+var Monitor = new Item("Flemitor", 7000.00, "3 Business Days")
