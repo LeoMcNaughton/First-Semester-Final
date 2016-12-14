b63fe6571700f1eba53759f5f956fdbf6b1fbf59
LeoMcNaughton <9lmcnaughton@stpatrick.org>
2016-12-14 11:23:50 -0600

    Add files via upload

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -9,16 +9,26 @@ constructor (iL, iQ){
 
   //Type the instance functions below this comment
 additem(i,q){
-  
+  this.itemList.push(i);
+  this.itemQuantity.push(q);
 }
 
 
 totalCart(){
-
+let total = 0;
+for(a=0;a<this.itemList.length;a++){
+  total += this.itemList[a].price*this.itemQuantity[a];
+}
+return total;
 }
 subCart(days){
-
-
+let subCart = new Cart([],[]);
+for(b=0;b>this.sitemList.length;a++){
+  if(this.itemList[b].shipping == days){
+    subCart.push([b]);
+  }
+}
+return subCart;
 }
 
 }
